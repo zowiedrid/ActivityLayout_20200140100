12b2716e4b278fef99733623259843d9768f7bd8
ilham fadhilah <ilham.fadhilah.ft20mail.umy.ac.id>
2023-10-06 15:39:33 +0700

    final

---
+++ app/src/main/java/com/example/buburubub/activity_linear_layout.kt
@@ -0,0 +1,11 @@
+package com.example.buburubub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_linear_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
